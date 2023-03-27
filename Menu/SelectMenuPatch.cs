f1d06bdef5ff34a30aba80c7fbe693ef71e5aaa7
Metnias <40355580+metnias@users.noreply.github.com>
2023-03-27 17:18:23 +0900

    More WIP select menu

--- Menu/SelectMenuPatch.cs
+++ Menu/SelectMenuPatch.cs
@@ -1,4 +1,9 @@
-﻿using Menu;
+﻿using JollyCoop.JollyMenu;
+using Menu;
+using RWCustom;
+using System;
+using System.Linq;
+using UnityEngine;
 using static LancerRemix.LancerEnums;
 using static Menu.SlugcatSelectMenu;
 using SlugName = SlugcatStats.Name;
@@ -9,10 +14,13 @@ namespace LancerRemix.Menu
     {
         internal static void MiniPatch()
         {
-            On.Menu.SlugcatSelectMenu.ctor += SlugcatSelectMenuCtorPatch;
+            On.Menu.SlugcatSelectMenu.ctor += CtorPatch;
+            On.Menu.SlugcatSelectMenu.Update += UpdatePatch;
+            On.Menu.SlugcatSelectMenu.Singal += SignalPatch;
+            On.Menu.SlugcatSelectMenu.SlugcatPage.GrafUpdate += PageGrafUpdatePatch;
         }
 
-        private static void SlugcatSelectMenuCtorPatch(On.Menu.SlugcatSelectMenu.orig_ctor orig, SlugcatSelectMenu self, ProcessManager manager)
+        private static void CtorPatch(On.Menu.SlugcatSelectMenu.orig_ctor orig, SlugcatSelectMenu self, ProcessManager manager)
         {
             orig(self, manager);
             slugcatPageLancer = false;
@@ -35,7 +43,9 @@ namespace LancerRemix.Menu
                 UpdateCurrentlySelectedLancer(lancer, order);
             }
             // Add Toggle Button
-
+            lancerButton = new SymbolButtonToggle(self, self.pages[0], LANCER_SIGNAL, new Vector2(1116f, 50f), new Vector2(50f, 50f),
+                "Big_Menu_Arrow", "Kill_Slugcat", slugcatPageLancer, false); // TODO: replace sprites
+            self.pages[0].subObjects.Add(lancerButton);
 
             int GetBasisOrder(SlugName lancer)
             {
@@ -49,7 +59,7 @@ namespace LancerRemix.Menu
             {
                 if (self.manager.rainWorld.progression.miscProgressionData.currentlySelectedSinglePlayerSlugcat == lancer)
                 {
-                    slugcatPageLancer = true;
+                    slugcatPageLancer = true; lancerTransition = 1f; lastLancerTransition = 1f;
                     self.slugcatPageIndex = order;
                 }
             }
@@ -57,41 +67,101 @@ namespace LancerRemix.Menu
 
         private static SlugcatPage[] lancerPages;
         private static bool slugcatPageLancer;
+        private static SymbolButtonToggle lancerButton;
+        private static float lancerTransition = 0f;
+        private static float lastLancerTransition = 0f;
 
         private static bool IsLancerPage(SlugcatPage page)
             => page is LancerPageNewGame || page is LancerPageContinue;
 
-    }
+        internal const float VOFFSET = 800f;
+        private static readonly string LANCER_SIGNAL = "LANCER";
 
-    internal class LancerPageNewGame : SlugcatPageNewGame
-    {
-        public LancerPageNewGame(global::Menu.Menu menu, MenuObject owner, int pageIndex, SlugName lancerNumber) : base(menu, owner, pageIndex, GetBasis(lancerNumber))
+        private static void UpdatePatch(On.Menu.SlugcatSelectMenu.orig_Update orig, SlugcatSelectMenu self)
         {
-            basisNumber = slugcatNumber;
-            slugcatNumber = lancerNumber;
+            orig(self);
+            lastLancerTransition = lancerTransition;
+            lancerTransition = Custom.LerpAndTick(lancerTransition, slugcatPageLancer ? 1f : 0f, 0.2f, 0.1f);
         }
 
-        internal SlugName basisNumber;
-
-    }
-
-    internal class LancerPageContinue : SlugcatPageContinue
-    {
-        public LancerPageContinue(global::Menu.Menu menu, MenuObject owner, int pageIndex, SlugName lancerNumber) : base(menu, owner, pageIndex, GetBasis(lancerNumber))
+        private static void SignalPatch(On.Menu.SlugcatSelectMenu.orig_Singal orig, SlugcatSelectMenu self, MenuObject sender, string message)
         {
-            basisNumber = slugcatNumber;
-            slugcatNumber = lancerNumber;
-        }
-
-        internal SlugName basisNumber;
-
-        public new SaveGameData saveGameData
-        {
-            get
+            if (message == LANCER_SIGNAL)
             {
-                return (menu as SlugcatSelectMenu).saveGameData[slugcatNumber];
+                slugcatPageLancer = !slugcatPageLancer;
+                self.PlaySound(SoundID.MENU_Next_Slugcat);
+                return;
+            }
+            orig(self, sender, message);
+        }
+
+        private static void PageGrafUpdatePatch(On.Menu.SlugcatSelectMenu.SlugcatPage.orig_GrafUpdate orig, SlugcatPage self, float timeStacker)
+        {
+            orig(self, timeStacker);
+            float offset = Mathf.Lerp(lastLancerTransition, lancerTransition, timeStacker) * VOFFSET;
+            //if (IsLancerPage(self)) offset -= VOFFSET;
+            if (self.markSquare != null && self.markGlow != null)
+            {
+                self.markSquare.y += offset;
+                self.markGlow.y += offset;
+            }
+            if (self.glowSpriteA != null && self.glowSpriteB != null)
+            {
+                self.glowSpriteB.y += offset;
+                self.glowSpriteA.y += offset;
+            }
+            foreach (var illust in self.slugcatImage.depthIllustrations)
+                illust.pos.y += offset;
+            foreach (var illust in self.slugcatImage.flatIllustrations)
+                illust.pos.y += offset;
+            if (self is SlugcatPageNewGame pageNew)
+            {
+                float o = pageNew.infoLabel.text.Contains("\n") ? 30f : 0f;
+                pageNew.difficultyLabel.label.y = (self.imagePos.y - 249f + o) + offset;
+                pageNew.infoLabel.label.y = (self.imagePos.y - 249f - 60f + o / 2f) + offset;
+            }
+            else if (self is SlugcatPageContinue pageCont)
+            {
+                pageCont.regionLabel.label.y = (self.imagePos.y - 249f) + offset;
             }
         }
 
+
+        internal class LancerPageNewGame : SlugcatPageNewGame
+        {
+            public LancerPageNewGame(global::Menu.Menu menu, MenuObject owner, int pageIndex, SlugName lancerNumber) : base(menu, owner, pageIndex, GetBasis(lancerNumber))
+            {
+                basisNumber = slugcatNumber;
+                slugcatNumber = lancerNumber;
+
+                sceneOffset.y -= VOFFSET;
+            }
+
+            internal SlugName basisNumber;
+
+        }
+
+        internal class LancerPageContinue : SlugcatPageContinue
+        {
+            public LancerPageContinue(global::Menu.Menu menu, MenuObject owner, int pageIndex, SlugName lancerNumber) : base(menu, owner, pageIndex, GetBasis(lancerNumber))
+            {
+                basisNumber = slugcatNumber;
+                slugcatNumber = lancerNumber;
+
+                sceneOffset.y -= VOFFSET;
+            }
+
+            internal SlugName basisNumber;
+
+            public new SaveGameData saveGameData
+            {
+                get
+                {
+                    return (menu as SlugcatSelectMenu).saveGameData[slugcatNumber];
+                }
+            }
+
+        }
     }
+
 }
