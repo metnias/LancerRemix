a6caab727a229f75933cc1659554738dac99312b
Metnias <40355580+metnias@users.noreply.github.com>
2023-03-29 17:22:49 +0900

    Added plan for the future

--- LancerMenu/MenuModifier.cs
+++ LancerMenu/MenuModifier.cs
@@ -13,6 +13,10 @@ namespace LancerRemix.LancerMenu
             SelectMenuPatch.MiniPatch();
             /// TODO: add Lancer toggle button in slugcat select menu
             /// clicking that swooshes illust upwards.
+            /// 
+            /// or just save which players should be lancer
+            /// and attach supplements to them separately
+            /// lancer supplements will pass null for orig to prevent double updates
         }
     }
 }
